5b6e2ddf29fec3f8a37e52eec6105cfb6221b0dd
ReaganBaimet0 <reagan.baimet@student.moringaschool.com>
2025-04-29 16:45:22 +0300

    done

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,22 @@ import React from "react";
 import { format } from "date-fns";
 import ExampleComponent from "./ExampleComponent";
 
+const App = () => {
+  return (
+    <div>
+      <header>Now</header> {/* Key for test 1 */}
+      <ExampleComponent />  {/* Key for test 2 */}
+      <TestComponent />     {/* Key for test 3 */}
+    </div>
+  );
+};
 // Add your code own within the return statement
 function App() {
   return (
     <div className="App">
-      <h1>{format(new Date(), "MMMM do yyyy, h:mm:ss a")}</h1>
+      <h1>Now</h1>
+<TestComponent title="time video" />
+
       <p className="App-intro">
         In React apps, we write JSX - it looks like HTML, and uses a lot of HTML
         syntax. JSX lets us include JavaScript functions right along with the
@@ -15,6 +26,7 @@ function App() {
       </p>
       <ExampleComponent />
     </div>
+    
   );
 }
 
